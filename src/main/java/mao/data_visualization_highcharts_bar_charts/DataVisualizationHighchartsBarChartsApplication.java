fd52cb27c50b0f872fec88ae62e2157d9fa93b0a
mao <1296193245@qq.com>
2023-06-10 23:55:31 +0800

    data_visualization_highcharts_bar_charts

---
+++ src/main/java/mao/data_visualization_highcharts_bar_charts/DataVisualizationHighchartsBarChartsApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_bar_charts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationHighchartsBarChartsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationHighchartsBarChartsApplication.class, args);
+    }
+
+}
